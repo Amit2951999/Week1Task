d4707b14ddf71d3262337f516948555779f78873
Amit2951999 <50862874+Amit2951999@users.noreply.github.com>
2022-11-05 17:30:54 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     source_bucket = event['Records'][0]['s3']['bucket']['name']
-    object_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+    source_object_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
     response=s3.get_object(Bucket=source_bucket,Key=object_key)
     target_bucket = 'my-student-backup-data'
     copy_source = {'Bucket': source_bucket, 'Key': object_key}
