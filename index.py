0ce02492bfaf5dc51ee313373dbdfcd50e257c99
Amit2951999 <50862874+Amit2951999@users.noreply.github.com>
2022-11-07 18:06:17 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 import urllib
 import boto3
-import json
 
 s3 = boto3.client('s3')
 
@@ -9,10 +8,10 @@ def lambda_handler(event, context):
     source_bucket = event['Records'][0]['s3']['bucket']['name']
     source_object_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
     target_bucket = 'my-student-backup-data'
-    copy_source = {'Bucket': source_bucket, 'Key': object_key}
+    copy_source = {'Bucket': source_bucket, 'Key': source_object_key}
 
     try:
-        s3.copy_object(Bucket=target_bucket, Key=object_key, CopySource=copy_source)
+        s3.copy_object(Bucket=target_bucket, Key=source_object_key, CopySource=copy_source)
     except Exception as err:
         print("Error -" + str(err))
         return err
